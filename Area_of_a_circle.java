2fdf11bf675949e97af6228584bc32fcf79a63f8
sireesha <97424637+21A91A05F2@users.noreply.github.com>
2023-02-01 20:30:55 +0530

    import java.util.*; class cir{     public static void main(String args[]){         int r;         Scanner sc=new Scanner(System.in);         r = sc.nextInt();         Double a = 3.14*(r)*(r);         System.out.format("%.2f",a);     } }

---
+++ Area_of_a_circle.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class cir{
+    public static void main(String args[]){
+        int r;
+        Scanner sc=new Scanner(System.in);
+        r = sc.nextInt();
+        Double a = 3.14*(r)*(r);
+        System.out.format("%.2f",a);
+    }
+}
